51973a65134172c66793ad8f8eb200bc6aa31c67
Huctica <nfn6789@gmail.com>
2021-03-29 17:49:43 -0700

    Completed  MiCard section. Modified the end result with a different color scheme, padding, and width constraints so that the page renders better

--- lib/main.dart
+++ lib/main.dart
@@ -9,41 +9,92 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.deepOrange.shade900,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                color: Colors.red,
-                child: Text("container1"),
-                width: 100,
-                height: double.infinity,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Container(
+                constraints: BoxConstraints(maxWidth: 800),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 60,
+                      backgroundImage:
+                          AssetImage('images/genericprofilepicture.png'),
+                    ),
+                    Text(
+                      "Ratitack",
+                      style: TextStyle(
+                          fontFamily: 'Sansita',
+                          color: Colors.white,
+                          fontSize: 50,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    Text(
+                      "Flutter Developer", // would be cool to have this transition to a bunch of different role titles I would like to have
+                      style: TextStyle(
+                        fontFamily: 'Epilogue',
+                        fontSize: 15,
+                        fontWeight: FontWeight.w100,
+                        color: Colors.white,
+                        letterSpacing: 2.5,
+                      ),
+                    ),
+                    Container(
+                      color: Colors.black38,
+                      padding: EdgeInsets.all(10),
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.phone,
+                            size: 60,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "+1 233 456 7890",
+                            style: TextStyle(
+                                fontFamily: 'Epilogue',
+                                color: Colors.white,
+                                fontSize: 30),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      color: Colors.black38,
+                      padding: EdgeInsets.all(10),
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.email,
+                            size: 60,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "person@anemail.com",
+                            style: TextStyle(
+                                fontFamily: 'Epilogue',
+                                color: Colors.white,
+                                fontSize: 30),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.yellow,
-                    child: Text("container2"),
-                    height: 100,
-                    width: 100,
-                  ),
-                  Container(
-                    color: Colors.green,
-                    child: Text("container4"),
-                    height: 100,
-                    width: 100,
-                  ),
-                ],
-              ),
-              Container(
-                color: Colors.blue,
-                child: Text("container3"),
-                width: 100,
-                height: double.infinity,
-              ),
-            ],
+            ),
           ),
         ),
       ),
